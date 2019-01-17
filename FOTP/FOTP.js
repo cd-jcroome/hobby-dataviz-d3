cee6564f4f802d57fd83e90528021554e7684f57
Jasparr77 <jjcroome@gmail.com>
2019-01-17 06:38:53 -0800

    Update FOTP.js
    urgh

--- FOTP/FOTP.js
+++ FOTP/FOTP.js
@@ -17,9 +17,19 @@ var chartGroup = svg.append("g")
     
 d3.csv('https://gist.githubusercontent.com/Jasparr77/0e278e24b4b8af013f2ba6d71ec0c979/raw/caa81ddddcf1c39a682af435817498ca5c719a0b/FOTP.csv').then(function(data){
 
-// data.sort(function(d) {return d3.ascending(d.Edition);})
+var nested_data = d3.nest()
+.key(function(d){return d.Country;})
+.key(function(d){return d.Edition;}).sortKeys(d3.ascending)
 
-console.log(data)
+.rollup(function(leaves){return {
+    Total_Score: d3.sum(leaves, function(d){return d['Total Score'] ;}),
+    Status: d3.max(leaves, function(d){return d.Status ;})
+}})
+.entries(data)
+console.log(nested_data)
+	// .map(function(d){return {Country:d.key, Entry: d.value.key, totalScore:d.value.Total_Score, Status:d.value.Status};})
+// console.log(data)
+// console.log(nested_data)
 
 var y = d3.scaleLinear()
 .domain([0,100])
@@ -34,6 +44,23 @@ var yAxis = d3.axisLeft(y);
 
 var xAxis = d3.axisBottom(x);
 
+var line = d3.line()
+    .x(function(nested_data) { return x(nested_data.Edition); })
+    .y(function(d) { return y(+d.Total_Score) })
+    .curve(d3.curveNatural);
+
+var countries = chartGroup.selectAll(".countries")
+    .data(nested_data)
+    .enter()
+    .append("g")
+
+var paths = countries.selectAll(".line")
+    .data(function(d){
+        return d.values.Total_Score
+    } )
+    .enter()
+    .append("path");
+
 chartGroup.append("g")
 .attr("class","axis y")
 .call(yAxis)
@@ -42,5 +69,14 @@ chartGroup.append("g")
 .attr("class","axis x")
 .attr("transform","translate(0,"+mainheight+")")
 .call(xAxis)
+
+chartGroup.selectAll(".line")
+    .data(nested_data)
+    .enter()
+    .append("path")
+        .attr("class","line")
+        .attr("d", function(d){
+            return line(d.values)
+        })
 
 ;})
