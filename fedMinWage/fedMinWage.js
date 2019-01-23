aea0990e957f53f30b8e9dc98cd54913f7a19a9f
Jasparr77 <jjcroome@gmail.com>
2019-01-23 06:19:18 -0800

    Update fedMinWage.js
    not working yet

--- fedMinWage/fedMinWage.js
+++ fedMinWage/fedMinWage.js
@@ -13,25 +13,10 @@ var svg = d3.select(".mainviz").append("svg")
 var chartGroup = svg.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var chartGroup = d3.select("g");
-
 var path = d3.geoPath();
 
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-  if (error) throw error;
-
-  chartGroup.append("g")
-      .attr("class", "states")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path);
-
-  chartGroup.append("path")
-      .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-
-});
+var projection = d3.geoAlbersUsa()
+                    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv('https://gist.githubusercontent.com/Jasparr77/80574e8c8409ca34a9fd29f33cfc6be5/raw/f32645bbd5f7ed1faeed2fcb3f9c1b837200ec8a/minWage.tsv').then(function(data){
 console.log(data);})
