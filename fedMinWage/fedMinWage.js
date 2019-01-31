85f3b896124e51d6d1a07529ae355c28de9ea008
Jasparr77 <jjcroome@gmail.com>
2019-01-31 06:29:38 -0800

    working on that nest
    nest nest nest

--- fedMinWage/fedMinWage.js
+++ fedMinWage/fedMinWage.js
@@ -3,7 +3,7 @@ var margin = { top: 20, right: 20, bottom: 60, left: 50 };
 var mainwidth = (window.innerWidth - margin.left - margin.right),
     mainheight = (window.innerHeight*.6) - margin.top - margin.bottom;
 
-var w = mainwidth*.15, h = mainheight*.5;
+var w = mainwidth*.1, h = mainheight*.5;
 
 var svg = d3.select(".mainviz").append("svg")
 .attr("class","container")
@@ -12,6 +12,10 @@ var svg = d3.select(".mainviz").append("svg")
 
 var chartGroup = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var div = d3.select(".scroll__graphic").append("div")
+    .attr("class","tooltip")
+    .style("opacity","0")
 
 var path = d3.geoPath();
 
@@ -22,27 +26,33 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     console.log(data)
     var nested_data = d3.nest()
     .key(function(d){return d['State ID'];})
+    // .key(function(d){return d['State'];})
 	.rollup(function(leaves){return {
-        avgAt: d3.mean(leaves, function(d){return d['At Minimum Wage']}),
+        avgAt: d3.mean(leaves, function(d){return d['At Minimum Wage'] ;}),
 		avgTotal: d3.mean(leaves, function(d){return d['Total'] ;}),
 		avgBelow: d3.mean(leaves, function(d){return d['Below Minimum Wage'] ;})
 	}})
 	.entries(data)
-    .map(function(d){return {ID:d.key, Total:d.value.avgTotal, At: d.value.avgAt, Below: d.value.avgBelow};})
+    .map(function(d){return {ID:d.key, Name: d.values.key,Total:d.values.value.avgTotal, At: d.values.value.avgAt, Below: d.values.value.avgBelow
+    };})
+
+console.log(nested_data)
 
 // join d3 & state data
 us.objects.states.geometries.forEach(function(us) {
     var result = nested_data.filter(function(nest) {
-        return nest.ID === us.id;
+        return nest.key === us.id;
     });
-    us.Total = (result[0] !== undefined) ? result[0].Total : null 
-    us.At = (result[0] !== undefined) ? result[0].At : null
-    us.Below = (result[0] !== undefined) ? result[0].Below : null
+    us.name = (result[0] !== undefined) ? result[0].State : null  
+    us.Total = (result[0] !== undefined) ? result[0].values.Total : null 
+    us.At = (result[0] !== undefined) ? result[0].values.Total : null
+    us.Below = (result[0] !== undefined) ? result[0].values.Below : null
     ;
 });
 
-console.log(nested_data, us)
+console.log(us)
 
+// Legend work
 var minVal = d3.min(nested_data,function(d){return d.Total;});
 var maxVal = d3.max(nested_data,function(d){return d.Total;});
 
@@ -51,6 +61,48 @@ var highColor = '#cc5500'
 
 var ramp = d3.scaleLinear().domain([minVal,maxVal]).range([lowColor,highColor]);
 
+var key = d3.select(".container")
+.append("svg")
+.attr("width", w)
+.attr("height", h)
+.attr("class", "legend");
+
+var legend = key.append("defs")
+.append("svg:linearGradient")
+.attr("id", "gradient")
+.attr("x1", "100%")
+.attr("y1", "0%")
+.attr("x2", "100%")
+.attr("y2", "100%")
+.attr("spreadMethod", "pad");
+
+legend.append("stop")
+.attr("offset", "0%")
+.attr("stop-color", highColor)
+.attr("stop-opacity", 1);
+
+legend.append("stop")
+.attr("offset", "100%")
+.attr("stop-color", lowColor)
+.attr("stop-opacity", 1);
+
+key.append("rect")
+.attr("width", w)
+.attr("height", h)
+.style("fill", "url(#gradient)")
+.attr("transform", "translate(0,10)")
+
+var y = d3.scaleLinear()
+.range([h, 0])
+.domain([minVal, maxVal]);
+
+var yAxis = d3.axisRight(y);
+
+key.append("g")
+.attr("class", "y axis")
+.attr("transform", "translate("+ w +",10)")
+.call(yAxis);
+// End Legend Work
 
 // State Bodies
 d3.select(".container")
@@ -69,7 +121,7 @@ d3.select(".container")
 	if (value) {//If value exists…
 	return ramp(value);
 	} else {//If value is undefined…
-    console.log(d)
+    // console.log(d)
     return "#ffffff";
     }
 })
@@ -77,7 +129,11 @@ d3.select(".container")
     div.transition()        
          .duration(200)      
        .style("opacity", .9);      
-       div.text(d.ID)
+       div.html(
+           d.id + "<br/>" 
+        //    + "Avg % at or below Minimum Wage | "
+           + d.Total.toLocaleString("en", {style: "percent"})
+        )
        .style("left", (d3.event.pageX) + "px")     
        .style("top", (d3.event.pageY - 28) + "px");    
 })   
@@ -99,48 +155,5 @@ svg.append("path")
 .attr("stroke-width",".05vw")
 .attr("fill","none");
 
-// Legend work
-    var key = d3.select(".container")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", "legend");
-
-    var legend = key.append("defs")
-        .append("svg:linearGradient")
-        .attr("id", "gradient")
-        .attr("x1", "100%")
-        .attr("y1", "0%")
-        .attr("x2", "100%")
-        .attr("y2", "100%")
-        .attr("spreadMethod", "pad");
-
-    legend.append("stop")
-        .attr("offset", "0%")
-        .attr("stop-color", highColor)
-        .attr("stop-opacity", 1);
-        
-    legend.append("stop")
-        .attr("offset", "100%")
-        .attr("stop-color", lowColor)
-        .attr("stop-opacity", 1);
-
-    key.append("rect")
-        .attr("width", w)
-        .attr("height", h)
-        .style("fill", "url(#gradient)")
-        .attr("transform", "translate(0,10)")
-
-    var y = d3.scaleLinear()
-        .range([h, 0])
-        .domain([minVal, maxVal]);
-
-    var yAxis = d3.axisRight(y);
-
-    key.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate("+ w +",10)")
-        .call(yAxis);
-// End Legend Work
 })
 })
