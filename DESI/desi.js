b7024650415d4bebaf635f2c14b809270c50f64f
Jasparr77 <jjcroome@gmail.com>
2019-02-10 09:39:53 -0800

    update
    fmw tooltip, added DESI

---
+++ DESI/desi.js
@@ -0,0 +1,33 @@
+function join(lookupTable, mainTable, lookupKey, mainKey, select) {
+    var l = lookupTable.length,
+        m = mainTable.length,
+        lookupIndex = [],
+        output = [];
+    for (var i = 0; i < l; i++) { // loop through l items
+        var row = lookupTable[i];
+        lookupIndex[row[lookupKey]] = row; // create an index for lookup table
+    }
+    for (var j = 0; j < m; j++) { // loop through m items
+        var y = mainTable[j];
+        var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
+        output.push(select(y, x)); // select only the columns you need
+    }
+    return output;
+};
+
+var margin = { top: 20, right: 20, bottom: 60, left: 50 };
+
+var mainwidth = (window.innerWidth - margin.left - margin.right),
+	mainheight = (window.innerHeight*.6) - margin.top - margin.bottom;
+
+var svg = d3.select(".mainviz").append("svg")
+.attr("class","container")
+.attr("width", mainwidth + margin.left + margin.right)
+.attr("height", mainheight + margin.top + margin.bottom);
+
+var chartGroup = svg.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv('https://gist.githubusercontent.com/Jasparr77/673faca63682a4c8788025ac021a46df/raw/9525eccf53d6a5c1248c9ff0cf925eb29040d5c1/desi.tsv',function(data){
+    console.log(data)
+;})
--- fedMinWage/fedMinWage.js
+++ fedMinWage/fedMinWage.js
@@ -15,7 +15,7 @@ var svg = d3.select(".mainviz").append("svg").attr("class", "container").attr("w
 
 var chartGroup = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var div = d3.select(".scroll__graphic").append("div").attr("class", "tooltip").style("opacity", "0").style("width", "40")
+var div = d3.select(".scroll__graphic").append("div").attr("class", "tooltip").style("opacity", "0").style("width", "40").style("position","absolute").style("text-align","center").style("background","lightsteelblue")
 
 var path = d3.geoPath();
 
