783859cffbeec27d7c5aee9d31c5bd7d5269cad4
Jasparr77 <jjcroome@gmail.com>
2019-02-11 06:09:07 -0800

    desi work
    yup

--- DESI/desi.js
+++ DESI/desi.js
@@ -1,21 +1,8 @@
-function join(lookupTable, mainTable, lookupKey, mainKey, select) {
-    var l = lookupTable.length,
-        m = mainTable.length,
-        lookupIndex = [],
-        output = [];
-    for (var i = 0; i < l; i++) { // loop through l items
-        var row = lookupTable[i];
-        lookupIndex[row[lookupKey]] = row; // create an index for lookup table
-    }
-    for (var j = 0; j < m; j++) { // loop through m items
-        var y = mainTable[j];
-        var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
-        output.push(select(y, x)); // select only the columns you need
-    }
-    return output;
-};
-
-var margin = { top: 20, right: 20, bottom: 60, left: 50 };
+var margin = { 
+    top: window.innerHeight*.02, 
+    right: window.innerWidth*.02, 
+    bottom: window.innerHeight*.02, 
+    left: window.innerWidth*.02 };
 
 var mainwidth = (window.innerWidth - margin.left - margin.right),
 	mainheight = (window.innerHeight*.6) - margin.top - margin.bottom;
@@ -28,6 +15,35 @@ var svg = d3.select(".mainviz").append("svg")
 var chartGroup = svg.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var div = d3.select(".scroll__graphic").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+    .style("position","absolute")
+    .style("text-align","center")
+    .style("background","whitesmoke")
+    .style("padding","8px")
+    .style("border-radius","none")
+    .style("pointer-events","none");
+
 d3.tsv('https://gist.githubusercontent.com/Jasparr77/673faca63682a4c8788025ac021a46df/raw/9525eccf53d6a5c1248c9ff0cf925eb29040d5c1/desi.tsv',function(data){
+    
+    var parseYear = d3.timeParse("%Y");
+    var formatYear = d3.timeFormat("%Y");
+
+    data.forEach(function(d){
+        d.Year = formatYear(
+            parseYear(d.Year)
+        );
+    })
+
+    var nested_data = d3.nest()
+    .key(function(d){return d.Country;})
+    .key(function(d){return d.Year;})
+    .rollup(function(leaves){return {
+        total_desi: d3.sum(leaves, function(d){return d['Weighted Score'];})
+	}})
+    .entries(data)
     console.log(data)
+    console.log(nested_data)
+
 ;})
