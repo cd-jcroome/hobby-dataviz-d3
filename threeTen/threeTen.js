30ba73c079150b38192a39860b990e805039376c
Jasparr77 <jjcroome@gmail.com>
2019-04-16 06:40:14 -0700

    Update threeTen.js
    progress - need to figure out cx, cy

--- threeTen/threeTen.js
+++ threeTen/threeTen.js
@@ -21,6 +21,9 @@ var formatPercent = d3.format(".0%")
 var formatDate = d3.timeFormat("%Y-%m-%e")
 var parseDate = d3.timeParse("%Y-%m-%e")
 
+var formatYear = d3.timeFormat("%Y")
+var formatWeek = d3.timeFormat("%U")
+
 var nestData = []
 var dict = []
 var weeks =[]
@@ -38,20 +41,17 @@ container.append("div")
     .style("z-index","9999");
 
 text.append("div")
-    .attr("id","formholder")
-    .html("<Form Name=\"userDOBForm\" onsubmit=\"getUserAge();return false\">When were you born?:<input type=\"date\"name=\"userDobInput\" ID=\"userDobInput\" min=\"01/01/1900\"max=\""+formatDate(new Date())+"\"><input type=\"submit\"value=\"Submit\"></Form>");    
-
-text.append("div")
     .attr("class", "step")
     .attr("data-step", "a")
-    .html("<h1>You've lived a shit-ton of weeks already</h1>")
-    .style("height",stepHeight +"px");
+    .attr("id","formholder")
+    .html("<Form Name=\"userDOBForm\" onsubmit=\"init();return false\">When were you born?:<input type=\"date\"name=\"userDobInput\" ID=\"userDobInput\" value=\""+formatDate(new Date())+"\" min=\""+formatDate(parseDate(1902-01-01))+"\"max=\""+formatDate(new Date())+"\"><input type=\"submit\"value=\"Submit\"></Form>");
 
 var tooltip = d3.select(".tooltip");
 
 function handleResize() {
     // 1. update height of step elements (moved to divs)
-
+    d3.select('.step')
+        .style("height",stepHeight + "px");
     // 2. update width/height of graphic element
     var bodyWidth = d3.select('body').node().offsetWidth;
     var textWidth = text.node().offsetWidth;
@@ -84,16 +84,20 @@ function getUserAge() {
     userDob = parseDate(document.getElementById('userDobInput').value)
     userDod = d3.timeDay.offset(userDob,(90*52*7))
     
-    weeks = d3.timeDay.range(userDob,userDod,7)
+    // weeks = d3.timeDay.range(userDob,userDod,7)
+    weeks = []
     weeksAlive = Math.floor(d3.timeDay.count(userDob,now)/7)
-    console.log(weeksAlive)
     for (var i = 0, len = (90*52); i < len; i++) {
-        wk = (i*7)
-        weeks.push([d3.timeDay.offset(userDob,wk),((i > Math.floor(d3.timeDay.count(userDob,now)/7)) ? "past" : "present")]);
+        wk = (i*7),
+        weeks.push([
+            d3.timeDay.offset(userDob,wk),
+            formatWeek(d3.timeWeek(d3.timeDay.offset(userDob,wk))),
+            formatYear(d3.timeYear(d3.timeDay.offset(userDob,wk)))-formatYear(d3.timeYear(userDob)),
+            (i < Math.floor(d3.timeDay.count(userDob,now)/7)+1) ? "past" : "present"
+    ]);
       }
 
-    console.log(weeks)
-// ask the user their DOB
+    console.log((d3.timeDay.count(userDob,now) < 1) ? "no date entered yet" : weeks)
 }
 
 // scrollama event handlers
@@ -101,15 +105,47 @@ function handleStepEnter(response) {
     // response = { element, direction, index }
     switch (response.index){
         case 0: 
-            var x = d3.scaleBand()
-                    .domain(['0','100'])
+            d3.selectAll(".title").remove()
+            d3.selectAll("circle").remove()
+            var x = d3.scaleLinear()
+                    .domain([d3.min(weeks, function(d){return d[2];}),d3.max(weeks, function(d){return d[2];})])
                     .range([0,xRange])
             var xAxis = d3.axisBottom(x).tickValues([]);
             
-            var y = d3.scaleBand()
-                    .domain(['0','52'])
-                    .range([yRange],0)
+            var y = d3.scaleLinear()
+                    .domain([d3.min(weeks, function(d){return d[1];}),d3.max(weeks, function(d){return d[1];})])
+                    .range([yRange,0])
             var yAxis = d3.axisLeft(y).tickValues([]);
+            
+            chartGroup.append("g")
+            .attr("class","axis y")
+            .attr("transform","translate("+(margin.left)+",0)")
+            .call(yAxis)
+        
+            chartGroup.append("g")
+            .attr("class","axis x")
+            .attr("transform","translate("+(margin.left)+"," + (Math.floor(window.innerHeight*.65)) + ")")
+            .call(xAxis)
+
+            graphic.append("text")
+                .attr("class","title")
+                .style("position","absolute")
+                .style("top","0%")
+                .style("right","50%")
+                .style("font-size","2vw")
+                .style("font-color","black")
+                .text((d3.timeDay.count(userDob,now) < 1) ? "When were you born?" : "wow.")
+            
+                graphic.selectAll("circle")
+                    .data(weeks)
+                    .enter().append("circle")
+                    .attr("class",function(d){return d[2]+d[1];})
+                    .attr("id",function(d){return d[0];})
+                    .attr("x",function(d){return x(d[2]);})
+                    .attr("y",function(d){return y(d[1]);})
+                    .attr("r",".02vw")
+                    .attr("fill","grey")
+                ;
 
         ; break;
     }
