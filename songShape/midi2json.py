2ff8c0ac3ee094705bb4d0b1b6f4d68e59b44cbc
Jasparr77 <jjcroome@gmail.com>
2019-06-06 06:53:33 -0700

    we did it!
    boom shakalaka - time for some dataviz

--- songShape/midi2json.py
+++ songShape/midi2json.py
@@ -1,5 +1,6 @@
 import py_midicsv
 import pandas as pd
+import numpy as np
 import json
 import csv
 from os import listdir
@@ -8,15 +9,33 @@ from os.path import isfile, join, splitext
 # for all of the mid or midi files in a given folder...
 files = [f for f in listdir('./songs') if isfile(join('./songs/',f))]
 print(files)
+
+d = {
+    'note':[0,7,2,9,4,11,6,1,8,3,10,5],
+    'note_name':['C','G','D','A','E','B','F#','C#','G#','D#','A#','F'],
+    'angle':[0,30,60,90,120,150,180,210,240,270,300,330,]}
+nmd = pd.DataFrame(data=d)
+nmd['note'] = pd.to_numeric(nmd['note'],errors='ignore').astype(int)
+print(nmd)
+
 ## convert midifile to pandas df
 for f in files:
     mc = py_midicsv.midi_to_csv(join('./songs/',f))
     mcdf = pd.DataFrame(mc)
-    print(mcdf[0])
+    mcdf = mcdf[0].str.split(',',expand=True).replace('\n','',regex=True)
+    
+        
+    mcdf['note_value'] = pd.to_numeric(mcdf[4],errors='coerce').fillna(0).astype(int)
+    
+# octave # will be radius - floor of (number divided by 12)
+# note will be angle - (remainder of (number divided by 12)) multiplied by the variable
+    mcdf['octave'],mcdf['note'] = mcdf['note_value']//12,mcdf['note_value']%12
+
+    mcdfx = mcdf.join(nmd,on='note',rsuffix='_nmd')
+
+    print(mcdfx)
 
 ## append note and octave data to the df
-## octave # will be radius - floor of (number divided by 12)
-# note will be angle - (remainder of (number divided by 12)) multiplied by the variable
 # note_angle = {'angles':[
 #     {0,{'note':'C','angle':0}},
 #     {7,{'note':'G','angle':30}},
@@ -32,9 +51,9 @@ for f in files:
 #     {5,{'note':'F','angle':330}}
 #     ]}
 
-    with open(join('./output/',splitext(f)[0],'.csv').replace("/.csv",".csv"),'w') as m_csv:
-        for row in mc:
-            m_csv.write(row)
+    m_csv = join('./output/',splitext(f)[0],'.csv').replace("/.csv",".csv")
+    
+    mcdfx.to_csv(m_csv, encoding='utf-8')
 
 # https://math.stackexchange.com/questions/260096/find-the-coordinates-of-a-point-on-a-circle
 # https://www.midimountain.com/midi/midi_note_numbers.html
