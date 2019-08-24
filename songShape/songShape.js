43d4493cb2d7127b3a9128457696c0e6e77b18da
Jasparr77 <jjcroome@gmail.com>
2019-08-24 06:36:08 -0700

    shrink
    remove excess whitespace

--- songShape/songShape.js
+++ songShape/songShape.js
@@ -1,157 +1,225 @@
-var container = d3.select('#staticBody')
+var container = d3.select("#staticBody");
 
-var margin = { top: (window.innerWidth*.14), right: 80, bottom: 60, left: 80 };
+var margin = { top: window.innerWidth * 0.14, right: 80, bottom: 60, left: 80 };
 
-container.append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0)
-    .style("position","absolute")
-    .style("text-align","center")
-    .style("background","whitesmoke")
-    .style("padding","8px")
-    .style("border-radius","8px")
-    .style("pointer-events","none")
-    .style("z-index","9999");
+container
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0)
+  .style("position", "absolute")
+  .style("text-align", "center")
+  .style("background", "whitesmoke")
+  .style("padding", "8px")
+  .style("border-radius", "8px")
+  .style("pointer-events", "none")
+  .style("z-index", "9999");
 
-container.append('svg').attr('class','chart');
+container.append("svg").attr("class", "chart");
 
 var tooltip = d3.select(".tooltip");
 var formHolder = d3.select("#formholder");
 var chartGroup = d3.select(".chart");
 
 function handleResize() {
-    var bodyWidth = (Math.floor(window.innerWidth*.95));
-    var bodyHeight = (Math.floor(window.innerHeight*.80))
-        
-    minDim = Math.min(bodyWidth, bodyHeight)
+  var bodyWidth = Math.floor(window.innerWidth * 0.95);
+  var bodyHeight = Math.floor(window.innerHeight * 0.8);
 
-    yRange = minDim
-    xRange = minDim
+  minDim = Math.min(bodyWidth, bodyHeight);
 
-    var chartMargin = 5;
-    chartWidth = xRange-chartMargin;
+  yRange = minDim;
+  xRange = minDim;
 
-    chartGroup
-        .style('width', chartWidth + 'px')
-        .style('height', bodyHeight + 'px');
+  var chartMargin = 5;
+  chartWidth = xRange - chartMargin;
+
+  chartGroup
+    .style("width", chartWidth + "px")
+    .style("height", bodyHeight + "px");
 }
-d3.csv('https://raw.githubusercontent.com/Jasparr77/hobby-dataviz-d3/master/songShape/output/Hallelujah.csv', function(data){
-    handleResize()
+d3.csv(
+  "https://raw.githubusercontent.com/Jasparr77/hobby-dataviz-d3/master/songShape/output/SevenNationArmy.csv",
+  function(data) {
+    console.log(data);
+    handleResize();
 
-    var x = d3.scaleLinear()
-            .domain([-9,9])
-            .range([0,xRange]);
+    var x = d3
+      .scaleLinear()
+      .domain([-9, 9])
+      .range([0, xRange]);
 
-    var y = d3.scaleLinear()
-            .domain([-9,9])
-            .range([yRange,0]);
+    var y = d3
+      .scaleLinear()
+      .domain([-9, 9])
+      .range([yRange, 0]);
 
-    var songPath = d3.line()
-        .curve(d3.curveBasis)
-        .x(function(d){return x(d.value['x'])})
-        .y(function(d){return y(d.value['y'])});
+    var songPath = d3
+      .line()
+      .curve(d3.curveBasis)
+      .x(function(d) {
+        return x(d.value["x"]);
+      })
+      .y(function(d) {
+        return y(d.value["y"]);
+      });
 
-    var color = d3.scaleOrdinal(d3.schemeCategory10)
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    anglePrep = function(d){
-        return d/180 * Math.PI
-    ;}
+    anglePrep = function(d) {
+      return (d / 180) * Math.PI;
+    };
 
-    var pointData = d3.nest()
-        .key(function(d){return d['']})
-        .rollup(function(leaves){
-            return {
-                x: d3.sum(leaves, function(d){return Math.sin(anglePrep(d['angle']))*d['octave']}), // x coordinate for note
-                y: d3.sum(leaves, function(d){return Math.cos(anglePrep(d['angle']))*d['octave']}), // y coordinate for note
-                channel: d3.max(leaves, function(d){return Number(d['channel'])}),
-                time: d3.max(leaves, function(d){return Number(d['note_seconds'])})
-            }
-        })
-        .entries(data)
-
-    var lineData = d3.nest()
-    .key(function(d){return d['channel']})
-    .key(function(d){return d['']})
-    .rollup(function(leaves){
+    var pointData = d3
+      .nest()
+      .key(function(d) {
+        return d[""];
+      })
+      .rollup(function(leaves) {
         return {
-            x: d3.sum(leaves, function(d){return Math.sin(anglePrep(d['angle']))*d['octave']}), // x coordinate for note
-            y: d3.sum(leaves, function(d){return Math.cos(anglePrep(d['angle']))*d['octave']}), // y coordinate for note
-            channel: d3.max(leaves, function(d){return Number(d['channel'])}),
-            time: d3.max(leaves, function(d){return Number(d['note_seconds'])})
-        }
-    })
-    .entries(data)
+          x: d3.sum(leaves, function(d) {
+            return Math.sin(anglePrep(d["angle"])) * d["octave"];
+          }), // x coordinate for note
+          y: d3.sum(leaves, function(d) {
+            return Math.cos(anglePrep(d["angle"])) * d["octave"];
+          }), // y coordinate for note
+          channel: d3.max(leaves, function(d) {
+            return Number(d["channel"]);
+          }),
+          time: d3.max(leaves, function(d) {
+            return Number(d["note_seconds"]);
+          })
+        };
+      })
+      .entries(data);
+
+    var lineData = d3
+      .nest()
+      .key(function(d) {
+        return d["chunk"];
+      })
+      .key(function(d) {
+        return d[""];
+      })
+      .rollup(function(leaves) {
+        return {
+          x: d3.sum(leaves, function(d) {
+            return Math.sin(anglePrep(d["angle"])) * d["octave"];
+          }), // x coordinate for note
+          y: d3.sum(leaves, function(d) {
+            return Math.cos(anglePrep(d["angle"])) * d["octave"];
+          }), // y coordinate for note
+          channel: d3.max(leaves, function(d) {
+            return Number(d["channel"]);
+          }),
+          time: d3.max(leaves, function(d) {
+            return Number(d["note_seconds"]);
+          })
+        };
+      })
+      .entries(data);
+
+    console.log(lineData);
 
     var noteData = [
-        {'note_name':'C','angle':0},
-        {'note_name':'G','angle':30},
-        {'note_name':'D','angle':60},
-        {'note_name':'A','angle':90},
-        {'note_name':'E','angle':120},
-        {'note_name':'B','angle':150},
-        {'note_name':'F#','angle':180},
-        {'note_name':'C#','angle':210},
-        {'note_name':'G#','angle':240},
-        {'note_name':'D#','angle':270},
-        {'note_name':'A#','angle':300},
-        {'note_name':'F','angle':330}
-    ]
+      { note_name: "C", angle: 0 },
+      { note_name: "G", angle: 30 },
+      { note_name: "D", angle: 60 },
+      { note_name: "A", angle: 90 },
+      { note_name: "E", angle: 120 },
+      { note_name: "B", angle: 150 },
+      { note_name: "F#", angle: 180 },
+      { note_name: "C#", angle: 210 },
+      { note_name: "G#", angle: 240 },
+      { note_name: "D#", angle: 270 },
+      { note_name: "A#", angle: 300 },
+      { note_name: "F", angle: 330 }
+    ];
 
-    lastRecord = data.length-1
+    lastRecord = data.length - 1;
 
-    chartGroup.selectAll(".notePoint")
-        .data(noteData)
-        .enter().append("text")
-        .attr("class","notePoint")
-        .attr("dy",".31em")
-        .attr("x",function(d){ return x((Math.sin(anglePrep(d['angle'])))*8); })
-        .attr("y",function(d){ return y((Math.cos(anglePrep(d['angle'])))*8); })
-        .text(function(d){ return d['note_name']; });
+    chartGroup
+      .selectAll(".notePoint")
+      .data(noteData)
+      .enter()
+      .append("text")
+      .attr("class", "notePoint")
+      .attr("dy", ".31em")
+      .attr("x", function(d) {
+        return x(Math.sin(anglePrep(d["angle"])) * 8);
+      })
+      .attr("y", function(d) {
+        return y(Math.cos(anglePrep(d["angle"])) * 8);
+      })
+      .text(function(d) {
+        return d["note_name"];
+      });
 
-    chartGroup.selectAll(".line")
-        .data(lineData)
-        .enter()
-        .append("path")
-        .attr("class",function(d){return d['key']+" songPath"})
-        .attr("d",function(d) {return songPath(d.values);})
-        .attr("fill",function(d){return color(Number(d['key']))})
-        .attr("fill-opacity",.2)
-        .attr("stroke",function(d){return color(Number(d['key']))})
-        .attr("stroke-opacity",.1)
-        .attr("stroke-width",".01vw");
+    chartGroup
+      .selectAll(".line")
+      .data(lineData)
+      .enter()
+      .append("path")
+      .attr("class", function(d) {
+        return d["key"] + " songPath";
+      })
+      .attr("d", function(d) {
+        return songPath(d.values);
+      })
+      .attr("fill", function(d) {
+        return color(Number(d["key"]));
+      })
+      .attr("fill-opacity", 0.2)
+      .attr("stroke", function(d) {
+        return color(Number(d["key"]));
+      })
+      .attr("stroke-opacity", 0.1)
+      .attr("stroke-width", ".01vw");
 
-        chartGroup.selectAll(".circleFifths")
-            .data(data)
-            .enter().append("circle")
-            .attr("class","circleFifths")
-            .attr("cx",x(0))
-            .attr("cy",y(0))
-            .attr("r",function(d){return y(Number(d['octave']+2))})
-            .attr("fill","none")
-            .attr("stroke","white")
-            .attr("opacity",1)
-            .attr("stroke-width",".02vw");  
+    // chartGroup
+    //   .selectAll(".circleFifths")
+    //   .data(data)
+    //   .enter()
+    //   .append("circle")
+    //   .attr("class", "circleFifths")
+    //   .attr("cx", x(0))
+    //   .attr("cy", y(0))
+    //   .attr("r", function(d) {
+    //     return y(Number(d["octave"] + 2));
+    //   })
+    //   .attr("fill", "none")
+    //   .attr("stroke", "white")
+    //   .attr("opacity", 1)
+    //   .attr("stroke-width", ".02vw");
 
-    chartGroup.selectAll(".noteCircle").data(pointData)
-        .enter()
-        .append("circle")
-        .attr("class","noteCircle")
-        .attr("cx",function(d){return x(d.value['x']); })
-        .attr("cy",function(d){return y(d.value['y']); })
-        .attr("r",".3vw")
-        .attr("fill",function(d){return color(d.value['channel']); })
-        .attr("fill-opacity","0")
-        .attr("stroke","none")
-        .transition()
-            .delay(function(d){return (d.value['time'])*1000; })
-            .attr("fill-opacity",.6)
-            .attr("stroke","white")
-            .attr("stroke-width",".04vw")
-            .attr("r",".6vw")
-        .transition()
-            .attr("fill-opacity","0")
-            .attr("stroke","none")
+    chartGroup
+      .selectAll(".noteCircle")
+      .data(pointData)
+      .enter()
+      .append("circle")
+      .attr("class", "noteCircle")
+      .attr("cx", function(d) {
+        return x(d.value["x"]);
+      })
+      .attr("cy", function(d) {
+        return y(d.value["y"]);
+      })
+      .attr("r", ".3vw")
+      .attr("fill", function(d) {
+        return color(d.value["channel"]);
+      })
+      .attr("fill-opacity", "0")
+      .attr("stroke", "none")
+      .transition()
+      .delay(function(d) {
+        return d.value["time"] * 1000;
+      })
+      .attr("fill-opacity", 0.6)
+      .attr("stroke", "white")
+      .attr("stroke-width", ".04vw")
+      .attr("r", ".6vw")
+      .transition()
+      .attr("fill-opacity", "0")
+      .attr("stroke", "none");
     //         .attr("r",".2vw")
-
-})
+  }
+);
 // https://math.stackexchange.com/questions/260096/find-the-coordinates-of-a-point-on-a-circle
