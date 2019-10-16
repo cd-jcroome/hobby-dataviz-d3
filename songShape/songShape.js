a464a696456f3ad144742b67e90b55bfab0ba6d2
Jasparr77 <jjcroome@gmail.com>
2019-10-16 06:41:03 -0700

    librosa
    m4a works!

--- songShape/songShape.js
+++ songShape/songShape.js
@@ -33,7 +33,7 @@ function handleResize() {
 }
 d3.csv(
   "https://raw.githubusercontent.com/Jasparr77/hobby-dataviz-d3/master/songShape/" +
-    "output/SevenNationArmy.csv",
+    "output/soWhat.csv",
   function(data) {
     console.log(data);
     handleResize();
@@ -83,7 +83,7 @@ d3.csv(
               return d.instrument;
             })
           )
-          .range([".5"]);
+          .range([".25"]);
 
         var noteData = [
           { note_name: "C", angle: 0 },
@@ -113,16 +113,18 @@ d3.csv(
             return {
               x: d3.sum(leaves, function(d) {
                 return (
-                  // Math.sin(anglePrep(d["angle"])) * (1 - 0.1 * Number(d["octave"]))
-                  Math.sin(anglePrep((d["note_seconds"] / timeLength) * 360)) *
-                  (d["note_midi_value"] / 84)
+                  Math.sin(anglePrep(d["angle"])) *
+                  (1 - 0.1 * Number(d["octave"]))
+                  // Math.sin(anglePrep((d["note_seconds"] / timeLength) * 360)) *
+                  // (d["note_midi_value"] / 84)
                 );
               }), // x coordinate for note
               y: d3.sum(leaves, function(d) {
                 return (
-                  // Math.cos(anglePrep(d["angle"])) * (1 - 0.1 * Number(d["octave"]))
-                  Math.cos(anglePrep((d["note_seconds"] / timeLength) * 360)) *
-                  (d["note_midi_value"] / 84)
+                  Math.cos(anglePrep(d["angle"])) *
+                  (1 - 0.1 * Number(d["octave"]))
+                  // Math.cos(anglePrep((d["note_seconds"] / timeLength) * 360)) *
+                  // (d["note_midi_value"] / 84)
                 );
               }), // y coordinate for note
               channel: d3.max(leaves, function(d) {
@@ -147,16 +149,18 @@ d3.csv(
             return {
               x: d3.sum(leaves, function(d) {
                 return (
-                  // Math.sin(anglePrep(d["angle"])) * (1 - 0.1 * Number(d["octave"]))
-                  Math.sin(anglePrep((d["note_seconds"] / timeLength) * 360)) *
-                  (d["note_midi_value"] / 84)
+                  Math.sin(anglePrep(d["angle"])) *
+                  (1 - 0.1 * Number(d["octave"]))
+                  // Math.sin(anglePrep((d["note_seconds"] / timeLength) * 360)) *
+                  // (d["note_midi_value"] / 84)
                 );
               }), // x coordinate for note
               y: d3.sum(leaves, function(d) {
                 return (
-                  // Math.cos(anglePrep(d["angle"])) * (1 - 0.1 * Number(d["octave"]))
-                  Math.cos(anglePrep((d["note_seconds"] / timeLength) * 360)) *
-                  (d["note_midi_value"] / 84)
+                  Math.cos(anglePrep(d["angle"])) *
+                  (1 - 0.1 * Number(d["octave"]))
+                  // Math.cos(anglePrep((d["note_seconds"] / timeLength) * 360)) *
+                  // (d["note_midi_value"] / 84)
                 );
               }), // y coordinate for note
               channel: d3.max(leaves, function(d) {
@@ -171,22 +175,22 @@ d3.csv(
 
         lastRecord = data.length - 1;
 
-        // chartGroup
-        //   .selectAll(".notePoint")
-        //   .data(noteData)
-        //   .enter()
-        //   .append("text")
-        //   .attr("class", "notePoint")
-        //   .attr("dy", ".31em")
-        //   .attr("x", function(d) {
-        //     return x(Math.sin(anglePrep(d["angle"])) * 0.95);
-        //   })
-        //   .attr("y", function(d) {
-        //     return y(Math.cos(anglePrep(d["angle"])) * 0.95);
-        //   })
-        //   .text(function(d) {
-        //     return d["note_name"];
-        //   });
+        chartGroup
+          .selectAll(".notePoint")
+          .data(noteData)
+          .enter()
+          .append("text")
+          .attr("class", "notePoint")
+          .attr("dy", ".31em")
+          .attr("x", function(d) {
+            return x(Math.sin(anglePrep(d["angle"])) * 0.95);
+          })
+          .attr("y", function(d) {
+            return y(Math.cos(anglePrep(d["angle"])) * 0.95);
+          })
+          .text(function(d) {
+            return d["note_name"];
+          });
         // // add circles
 
         // chartGroup
@@ -230,63 +234,63 @@ d3.csv(
           .attr("stroke-opacity", function(d) {
             return opacity(d["key"]);
           })
-          .attr("stroke-width", ".25vw");
-        // .on("mouseover", function(d) {
-        //   d3.select(this)
-        //     .attr("stroke-opacity", 0.8)
-        //     .attr("stroke-width", "1vw");
-        //   div
-        //     .transition()
-        //     .duration(400)
-        //     .style("opacity", 0.9);
-        //   div
-        //     .html(d["key"])
-        //     .style("left", d3.event.pageX - margin.left + "px")
-        //     .style("top", d3.event.pageY - margin.bottom + "px");
-        // })
-        // .on("mouseout", function(d) {
-        //   d3.select(this)
-        //     .attr("stroke-opacity", function(d) {
-        //       return opacity(d["key"]);
-        //     })
-        //     .attr("stroke-width", ".25vw");
-        //   div
-        //     .transition()
-        //     .duration(400)
-        //     .style("opacity", 0);
-        // });
+          .attr("stroke-width", ".25vw")
+          .on("mouseover", function(d) {
+            d3.select(this)
+              .attr("stroke-opacity", 0.8)
+              .attr("stroke-width", "1vw");
+            div
+              .transition()
+              .duration(400)
+              .style("opacity", 0.9);
+            div
+              .html(d["key"])
+              .style("left", d3.event.pageX - margin.left + "px")
+              .style("top", d3.event.pageY - margin.bottom + "px");
+          })
+          .on("mouseout", function(d) {
+            d3.select(this)
+              .attr("stroke-opacity", function(d) {
+                return opacity(d["key"]);
+              })
+              .attr("stroke-width", ".25vw");
+            div
+              .transition()
+              .duration(400)
+              .style("opacity", 0);
+          });
 
         // dots
-        //   chartGroup
-        //     .selectAll(".noteCircle")
-        //     .data(pointData)
-        //     .enter()
-        //     .append("circle")
-        //     .attr("class", "noteCircle")
-        //     .attr("cx", function(d) {
-        //       return x(d.value["x"]);
-        //     })
-        //     .attr("cy", function(d) {
-        //       return y(d.value["y"]);
-        //     })
-        //     .attr("r", ".3vw")
-        //     .attr("fill", function(d) {
-        //       return color(d["key"]);
-        //     })
-        //     .attr("fill-opacity", 0.2)
-        //     .attr("stroke", "none")
-        //     .transition()
-        //     .delay(function(d) {
-        //       return d.value["time"] * 1000;
-        //     })
-        //     .attr("fill-opacity", 1)
-        //     .attr("stroke", "white")
-        //     .attr("stroke-width", ".04vw")
-        //     .attr("r", ".4vw")
-        //     .transition()
-        //     .attr("fill-opacity", "0")
-        //     .attr("stroke", "none")
-        //     .attr("r", ".2vw");
+        chartGroup
+          .selectAll(".noteCircle")
+          .data(pointData)
+          .enter()
+          .append("circle")
+          .attr("class", "noteCircle")
+          .attr("cx", function(d) {
+            return x(d.value["x"]);
+          })
+          .attr("cy", function(d) {
+            return y(d.value["y"]);
+          })
+          .attr("r", ".3vw")
+          .attr("fill", function(d) {
+            return color(d["key"]);
+          })
+          .attr("fill-opacity", 0.2)
+          .attr("stroke", "none")
+          .transition()
+          .delay(function(d) {
+            return d.value["time"] * 1000;
+          })
+          .attr("fill-opacity", 1)
+          .attr("stroke", "white")
+          .attr("stroke-width", ".04vw")
+          .attr("r", ".4vw")
+          .transition()
+          .attr("fill-opacity", "0")
+          .attr("stroke", "none")
+          .attr("r", ".2vw");
       }
     );
   }
